35a4d38c830ab34301b6d0227d44c91db5f387bd
moonvent <terraboneystone@proton.me>
2022-12-23 17:40:45 +0100

    1) finalize the info menu; 2) Fixed bug with scroll in messanger widget;

--- src/telegram_client/frontend/gui/widgets/chat/messanger/dialog_scroller.py
+++ src/telegram_client/frontend/gui/widgets/chat/messanger/dialog_scroller.py
@@ -8,14 +8,17 @@ class DialogScroller:
         Class for setup scroll in every dialog, recover it after return to worked dialog, and other
     """
     vertical_scroll: QScrollBar = None
-    visited_dialogs: dict[Dialog, int] = None
+    visited_dialogs: dict[Dialog, tuple[int, int]] = None       # need tuple with two int, 
+                                                                # first it's a last watched state, second it's max, cause 
+                                                                # after changing the dialogs need to reset maximum
     
     def recover_scroll(self, 
                        old_dialog):
         """
             Recover scroll value in old dialog
         """
-        self.vertical_scroll.setValue(self.visited_dialogs[old_dialog])
+        self.vertical_scroll.setRange(0, self.visited_dialogs[old_dialog][1])
+        self.vertical_scroll.setValue(self.visited_dialogs[old_dialog][0])
 
     def set_scroll(self, 
                    new_dialog,
@@ -24,13 +27,13 @@ class DialogScroller:
             Add new dialog to list of visited dialogs
         """
         self.vertical_scroll.setValue(self.vertical_scroll.maximum())
-        self.visited_dialogs[new_dialog] = self.vertical_scroll.value()
+        self.visited_dialogs[new_dialog] = [self.vertical_scroll.value(), self.vertical_scroll.maximum()]
 
     def save_dialog_scroll_value(self, 
                                  old_dialog: Dialog):
         """
             Save scroll state after replace current dialog
         """
-        self.visited_dialogs[old_dialog] = self.vertical_scroll.value()
-
+        if old_dialog:
+            self.visited_dialogs[old_dialog] = [self.vertical_scroll.value(), self.vertical_scroll.maximum()]
 
--- src/telegram_client/frontend/gui/widgets/chat/messanger/info_menu.py
+++ src/telegram_client/frontend/gui/widgets/chat/messanger/info_menu.py
@@ -1,9 +1,14 @@
+from datetime import datetime, timedelta
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QHBoxLayout, QLabel
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout
 from telethon.tl.types import Dialog, User
+from telethon.utils import hachoir
 from src.config import AVATARS_FOLDER_PATH, INFO_MENU_AVATAR_SIZE, INFO_MENU_HEIGHT
 from src.telegram_client.frontend.gui._core_widget import _CoreWidget
 import os
+from src.services.load_internalization import _
+from zoneinfo import ZoneInfo
+from time import tzname
 
 
 class InfoMenu(_CoreWidget):
@@ -11,6 +16,11 @@ class InfoMenu(_CoreWidget):
     _dialog: Dialog = None
 
     dialog_avatar: QLabel = None
+
+    layout_for_title_and_status: QVBoxLayout = None
+
+    title_label: QLabel = None
+    online_status_label: QLabel = None
 
     def __init__(self, parent, user) -> None:
         self.user = user
@@ -28,7 +38,7 @@ class InfoMenu(_CoreWidget):
     def set_layout(self):
         self.widget_layout = QHBoxLayout(self)
         self.setLayout(self.widget_layout)
-        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setContentsMargins(5, 0, 0, 0)
         self.layout().setSpacing(0)
 
     def load_ui(self):
@@ -38,24 +48,75 @@ class InfoMenu(_CoreWidget):
 
     def load_new_dialog(self):
         self.add_avatar()
+        self.load_title_and_status()
 
     def add_avatar(self):
         if not self.dialog_avatar:
             self.dialog_avatar = QLabel(self)
+            self.dialog_avatar.setObjectName('info_menu__avatar')
+            self.layout().addWidget(self.dialog_avatar)
         avatar = QPixmap(os.path.join(AVATARS_FOLDER_PATH, f'{self.dialog.id}.jpg'))
         avatar = avatar.scaled(INFO_MENU_AVATAR_SIZE, INFO_MENU_AVATAR_SIZE)
         self.dialog_avatar.setPixmap(avatar)
         self.dialog_avatar.setFixedWidth(INFO_MENU_AVATAR_SIZE)
-        self.layout().addWidget(self.dialog_avatar)
+
+    def load_title_and_status(self):
+        self.add_layout_for_title_and_status()
+        self.add_title()
+        self.add_last_online_status()
 
     def add_layout_for_title_and_status(self):
-        ...
+        if not self.layout_for_title_and_status:
+            self.layout_for_title_and_status = QVBoxLayout(self)
+            self.layout_for_title_and_status.setSpacing(0)
+            self.layout_for_title_and_status.setContentsMargins(0, 0, 0, 0)
+            self.layout().addLayout(self.layout_for_title_and_status)
 
     def add_title(self):
-        ...
+        if not self.title_label:
+            self.title_label = QLabel(self)
+            self.title_label.setObjectName('info_menu__title_label')
+            self.layout_for_title_and_status.addWidget(self.title_label)
+
+        self.title_label.setText(self.dialog.title)
 
     def add_last_online_status(self):
-        ...
+        if not self.online_status_label:
+            self.online_status_label = QLabel(self)
+            self.online_status_label.setObjectName('info_menu__online_status_label')
+            self.layout_for_title_and_status.addWidget(self.online_status_label)
+
+        entity = self.dialog.entity
+
+        if not isinstance(entity, User):
+            self.online_status_label.setText(f'{entity.participants_count:_} ' + _('subscribers'))
+            return
+
+        if hasattr(entity.status, 'expires'):
+            self.online_status_label.setText(_('online'))
+
+        else:
+            current_tz = ZoneInfo(tzname[0])
+
+            last_online_date = datetime.fromtimestamp(int(entity.status.was_online.timestamp()), 
+                                                      ZoneInfo(tzname[0]))
+
+            time_between_now_and_last_online = datetime.now().replace(tzinfo=current_tz) - last_online_date
+            last_online_text = _('last_online_status')
+
+            if time_between_now_and_last_online > timedelta(days=7):
+                last_online_text += last_online_date.strftime('%H:%M %d.%m.%Y')
+
+            elif time_between_now_and_last_online > timedelta(days=1):
+                last_online_text += last_online_date.strftime('%H:%M %d.%m.%Y')
+
+            elif time_between_now_and_last_online > timedelta(hours=1):
+                last_online_text += last_online_date.strftime('%H:%M')
+
+            else:
+                last_online_text += last_online_date.strftime('%H:%M')
+
+            self.online_status_label.setText(last_online_text)
 
     # def add_smth_else(self):
     #     ...
